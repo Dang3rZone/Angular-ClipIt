8929f8fc180fa668e9480e2150979d92a5e5958d
Andres <andresrosalespinzon@gmail.com>
2023-10-11 18:39:28 +0200

    feat: added interface id for modal and destory it

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,17 +1,33 @@
 import { Injectable } from '@angular/core';
 
+interface IModal {
+  id: string;
+  visible: boolean;
+}
 @Injectable({
   providedIn: 'root',
 })
 export class ModalService {
-  private visible = false;
+  private modals: IModal[] = [];
   constructor() {}
 
-  isModalOpen(): boolean {
-    return this.visible;
+  register(id: string) {
+    this.modals.push({ id: id, visible: false });
   }
 
-  toggleModal(): void {
-    this.visible = !this.visible;
+  unregister(id: string) {
+    this.modals = this.modals.filter((modal) => modal.id !== id);
+  }
+
+  isModalOpen(id: string): boolean {
+    return !!this.modals.find((modal) => modal.id === id)?.visible;
+  }
+
+  toggleModal(id: string): void {
+    const modal = this.modals.find((modal) => modal.id === id);
+
+    if (modal) {
+      modal.visible = !modal.visible;
+    }
   }
 }
